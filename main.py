bcb42a49f841189401560b4b0a9c73c5652fd4d2
Emmanuel John Lacsamana <ejohnlacsamana@outlook.com>
2025-06-12 10:57:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import Workout
 from database import init_db
 import sqlite3
 
 app = FastAPI()
 init_db()
+
+
+# Allow frontend to access backend (adjust origins if needed later)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or set to ["https://yourfrontend.vercel.app"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/workouts")
 def add_workout (workout: Workout):
